f198259d8b791fb5b6568825e6e40fd1862f7a5c
arolihas <arolihas@gmail.com>
2020-12-02 05:11:47 +0000

    Good results without classes

--- graphnets.py
+++ graphnets.py
@@ -11,19 +11,19 @@ from stellargraph.layer import GCN, GraphSAGE
 def recall_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / possible_positives
+    recall = true_positives / (possible_positives + K.epsilon())
     return recall
 
 def precision_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / predicted_positives
+    precision = true_positives / (predicted_positives + K.epsilon())
     return precision
 
 def f1_m(y_true, y_pred):
     precision = precision_m(y_true, y_pred)
     recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall))
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def training_split(training):
     train_subjects, test_subjects = model_selection.train_test_split(
